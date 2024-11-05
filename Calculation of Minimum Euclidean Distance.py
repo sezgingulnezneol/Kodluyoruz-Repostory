29c8a2bab69c6612b77ed4a94831e697ab2ddb5a
sezgingulnezneol <sezgingulnezneol@gmail.com>
2024-11-05 16:16:36 +0300

    Update Calculation of Minimum Euclidean Distance.py

--- Calculation of Minimum Euclidean Distance.py
+++ Calculation of Minimum Euclidean Distance.py
@@ -1,24 +1,22 @@
-'''
 import math
 
-# Noktaları tanımlıyoruz
-points = [(1, 2), (4, 6), (5, 3), (2, 1), (7, 5)]
-
-# İki nokta arasındaki Öklid mesafesini hesaplayan fonksiyon
 def euclideanDistance(point1, point2):
-    return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+    x1, y1 = point1
+    x2, y2 = point2
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return distance
 
-# Mesafeleri saklamak için liste
+points = [(33, 42), (23, 14), (31, 22), (98, 36), (49, 123)]
+
 distances = []
 
-# Her nokta çifti arasındaki mesafeyi hesaplama
 for i in range(len(points)):
     for j in range(i + 1, len(points)):
         distance = euclideanDistance(points[i], points[j])
         distances.append(distance)
 
-# Minimum mesafeyi bulma
+
+
 min_distance = min(distances)
 
-print("Tüm noktalar arasındaki minimum mesafe:", min_distance)
-'''
+print("Minimum mesafe:", min_distance)
