a1cc838f3f888463dc8fd190cb8ffb2de78d7839
sezgingulnezneol <sezgingulnezneol@gmail.com>
2024-11-05 15:41:11 +0300

    Update Calculation of Minimum Euclidean Distance.py
    Chatgpt ile bir kod yazdırıldı. Github öğrenmeye devam ediyorum.

--- Calculation of Minimum Euclidean Distance.py
+++ Calculation of Minimum Euclidean Distance.py
@@ -1,2 +1,24 @@
+'''
+import math
 
-Deneme1
+# Noktaları tanımlıyoruz
+points = [(1, 2), (4, 6), (5, 3), (2, 1), (7, 5)]
+
+# İki nokta arasındaki Öklid mesafesini hesaplayan fonksiyon
+def euclideanDistance(point1, point2):
+    return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+
+# Mesafeleri saklamak için liste
+distances = []
+
+# Her nokta çifti arasındaki mesafeyi hesaplama
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        distance = euclideanDistance(points[i], points[j])
+        distances.append(distance)
+
+# Minimum mesafeyi bulma
+min_distance = min(distances)
+
+print("Tüm noktalar arasındaki minimum mesafe:", min_distance)
+'''
